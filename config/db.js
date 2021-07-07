bb0ab37de2f8a5e9e4a07094a66073638443848a
Rahul-Menaria <67948541+Rahul-Menaria@users.noreply.github.com>
2021-07-07 17:49:38 +0530

    Update db.js
    error solved

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 module.exports = connect = async () => {
 	try {
-		const response = await mongoose.connect("mongodb+srv://rahul:admin@cluster0.31dn9.mongodb.net/blogify?retryWrites=true&w=majority", {
+		const response = await mongoose.connect("mongodb+srv://<username:password>@cluster0.31dn9.mongodb.net/blogify?retryWrites=true&w=majority", {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
 			useFindAndModify: false,
